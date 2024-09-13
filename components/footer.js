2109d6d20dab2c56b68b3557daa6e0c34025488a
fifovalle <fifanaufal10@gmail.com>
2024-09-13 19:02:09 +0700

    Menambahkan footer

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,6 @@ const Footer = () => {
 
   return (
     <footer className="w-full bg-gradient-to-l from-[#121212] to-[#0a0a0a] p-8">
-      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x- text-center md:justify-between"></div>
       <hr className="my-8 border-blue-gray-50" />
       <Typography
         color="blue-gray"
