0b2e300fa38ad20df33556c253e7a4b729255c35
fifovalle <fifanaufal10@gmail.com>
2024-09-13 08:15:24 +0700

    Merubah jadi js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,4 +15,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
